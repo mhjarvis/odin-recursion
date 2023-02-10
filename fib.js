be4dc6432982b0c8a545d55090781163fa756b9d
Markus <markus.h.jarvis@gmail.com>
2023-02-10 09:34:05 -0600

    add recursive print-out

--- fib.js
+++ fib.js
@@ -27,4 +27,14 @@ function fibs(n) {
   return arr;
 }
 
-console.log('SOLUTION 2: RECURSIVE FIBONACCI SEQUENCE');
+console.log('\nSOLUTION 2: RECURSIVE FIBONACCI SEQUENCE');
+console.log('----------------------------------------');
+console.log('A Fibonacci Sequence of 0 gives us: ' + fibsRec(0));
+console.log('A Fibonacci Sequence of 1 gives us: ' + fibsRec(1));
+console.log('A Fibonacci Sequence of 2 gives us: ' + fibsRec(2));
+console.log('A Fibonacci Sequence of 10 gives us: ' + fibsRec(10));
+console.log('A Fibonacci Sequence of 20 gives us: ' + fibsRec(20));
+
+function fibsRec(n) {
+  return fibs(n);
+}
